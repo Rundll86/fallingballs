ed5460f5a5151a2c9e2735af50d67178065b7816
陨落基围虾 <3161880837@qq.com>
2025-10-05 17:07:47 +0800

    feat(球类): 添加分裂球功能及调整相关场景
    实现分裂球功能，当球体攻击墙壁时可自我复制
    调整World场景使用新分裂球并更新碰撞层
    修改Winner场景尺寸和碰撞层配置
    更新BallBase基础属性以支持新功能

---
+++ scripts/Contents/Balls/SplitBall.gd
@@ -0,0 +1,9 @@
+extends BallBase
+
+var canDuplicate: bool = true
+func onAttack(_wall: WallBase):
+	if canDuplicate:
+		var cloned = duplicate()
+		get_parent().call_deferred("add_child", cloned)
+		cloned.position -= Vector2(0, radius * 2)
+	canDuplicate = false
