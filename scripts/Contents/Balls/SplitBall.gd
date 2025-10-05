c8d19cd44ef231467c3a8a6cf333a2f2803352b3
陨落基围虾 <3161880837@qq.com>
2025-10-05 17:42:27 +0800

    fix(Balls): 修复SplitBall重复克隆导致性能问题
    将重复克隆两次改为只克隆一次，避免因过多克隆体造成的卡顿

--- scripts/Contents/Balls/SplitBall.gd
+++ scripts/Contents/Balls/SplitBall.gd
@@ -4,7 +4,7 @@ var canDuplicate: bool = true
 
 func onAttack(_wall: WallBase):
 	if canDuplicate:
-		for i in 2: clone(Vector2(0, radius * -2))
+		clone(Vector2(0, radius * -2)) # 还是不要克隆2次了，太卡了
 		canDuplicate = false
 func getDamage():
 	return bounceTime ** 2
