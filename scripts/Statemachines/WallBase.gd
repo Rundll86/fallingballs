a8fa36d2ad0859b6fe269b4a0002e730e29053bd
陨落基围虾 <3161880837@qq.com>
2025-10-05 15:24:30 +0800

    feat(游戏场景): 添加伤害球场景和胜利着色器，重构墙壁和胜利区域
    重构墙壁基础类，添加生命值系统和等级机制
    修改胜利区域使用着色器效果
    调整重力盒场景布局，添加多层墙壁和胜利区域

--- scripts/Statemachines/WallBase.gd
+++ scripts/Statemachines/WallBase.gd
@@ -5,15 +5,25 @@ class_name WallBase
 @export var size: Vector2 = Vector2.ONE
 @export var color: Color = Color.GRAY
 @export var anchor: Vector2 = Vector2.ZERO
+@export var maxHealthBase: float = 100
+@export var level: int = 0
 
 @onready var hitbox: CollisionShape2D = $"%hitbox"
 @onready var texture: AnchoredPolygon = $"%texture"
+@onready var healthBar: Label = $"%health"
+var currentHealth: float = 0
 
 func _ready():
 	hitbox.shape = hitbox.shape.duplicate()
+	currentHealth = applyLevel(level + 1)
 func _process(_delta):
 	position = get_viewport_rect().size * anchor
 	texture.anchor = ShapeUtil.rect(size)
 	texture.position = get_viewport_rect().size * size / -2
 	texture.color = color
 	hitbox.shape.size = get_viewport_rect().size * size
+	healthBar.text = str(currentHealth)
+	healthBar.size = get_viewport_rect().size * size
+	healthBar.position = get_viewport_rect().size * size / -2
+func applyLevel(lv):
+	return maxHealthBase ** lv
--- scripts/Statemachines/Winner.gd
+++ scripts/Statemachines/Winner.gd
@@ -6,12 +6,12 @@ class_name Winner
 @export var anchor: Vector2 = Vector2.ZERO
 
 @onready var hitbox: CollisionShape2D = $"%hitbox"
-@onready var texture: AnchoredPolygon = $"%texture"
+@onready var texture: ColorRect = $"%texture"
 
 func _ready():
 	hitbox.shape = hitbox.shape.duplicate()
 func _process(_delta):
-	texture.anchor = ShapeUtil.rect(size)
-	texture.position = get_viewport_rect().size * size / -2
+	texture.size = get_viewport_rect().size * size
+	texture.position = texture.size / -2
 	hitbox.shape.size = get_viewport_rect().size * size
 	position = get_viewport_rect().size * anchor
