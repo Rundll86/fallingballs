6a7168eb74b9daee5e1955258ec1d2196e3eaa52
陨落基围虾 <3161880837@qq.com>
2025-10-05 17:41:33 +0800

    fix(SplitBall): 修复克隆球体时只生成一个的问题
    修改克隆逻辑，现在每次攻击会生成两个球体，以符合预期行为

--- scripts/Contents/Balls/SplitBall.gd
+++ scripts/Contents/Balls/SplitBall.gd
@@ -4,7 +4,7 @@ var canDuplicate: bool = true
 
 func onAttack(_wall: WallBase):
 	if canDuplicate:
-		clone(Vector2(0, radius * -2))
+		for i in 2: clone(Vector2(0, radius * -2))
 		canDuplicate = false
 func getDamage():
 	return bounceTime ** 2
