8c6e16b580c30d297360a137b410a3715a3d491e
陨落基围虾 <3161880837@qq.com>
2025-10-06 09:22:13 +0800

    feat(音效): 为挖掘球添加攻击音效资源
    添加Engine.wav音效文件及其导入配置，并在DrillBall场景中设置攻击音效节点

--- scripts/Contents/Balls/DrillBall.gd
+++ scripts/Contents/Balls/DrillBall.gd
@@ -6,7 +6,7 @@ var penerate = 0.1
 
 func onAttack(_wall: WallBase):
 	for i in floor(count):
-		get_parent().call_deferred("add_child", BulletBase.create("Drill", self, position - Vector2((radius * count) ** (1.0 / 1.1) * randf_range(-1, 1), 0), deg_to_rad(90)))
+		get_parent().call_deferred("add_child", BulletBase.create("Drill", self, position - Vector2((radius * count) ** (1.0 / 1.5) * randf_range(-1, 1), 0), deg_to_rad(90)))
 func getDamage():
 	count = bounceTime * 0.2 + 1
 	penerate = clamp(bounceTime * 0.005, 0, 1)
