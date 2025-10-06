7d88d43f2c44c35a924374b78f55b70c062182e0
陨落基围虾 <3161880837@qq.com>
2025-10-06 09:23:14 +0800

    fix(DrillBall): 调整钻头球的伤害和穿透计算参数
    将count的计算系数从0.2改为0.15，penerate的计算系数从0.005改为0.01，以平衡游戏难度

--- scripts/Contents/Balls/DrillBall.gd
+++ scripts/Contents/Balls/DrillBall.gd
@@ -8,8 +8,8 @@ func onAttack(_wall: WallBase):
 	for i in floor(count):
 		get_parent().call_deferred("add_child", BulletBase.create("Drill", self, position - Vector2((radius * count) ** (1.0 / 1.5) * randf_range(-1, 1), 0), deg_to_rad(90)))
 func getDamage():
-	count = bounceTime * 0.2 + 1
-	penerate = clamp(bounceTime * 0.005, 0, 1)
+	count = bounceTime * 0.15 + 1
+	penerate = clamp(bounceTime * 0.01, 0, 1)
 	return bounceTime + 1
 func applyInfo():
 	return "伤害：%s，钻头：%s，穿透：%s" % [
